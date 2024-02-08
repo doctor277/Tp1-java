ba4ee96a7527df6e21f7a158ae8f1e61f6fe99ab
doctor277 <dahoui_rafi@hotmail.com>
2024-02-07 20:47:30 -0500

    yesbaby

--- src/Hero.java
+++ src/Hero.java
@@ -17,8 +17,29 @@ public class Hero {
         this.damageInflicted = damageInflicted;
     }
 
+    // im not sure if its right or no 
+    // but iguess we're missing getters and setters
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void setHealth(int newHealth){
+        this.health = newHealth;
+    }
+     
+    public int getAttackPoints(){
+        return this.attackPoints;
+    }
+    
+    public void setAttackPoints(int newAttackPoints){
+        this.attackPoints = newAttackPoints;
+    }
+
+
+
     public void takeDamage(int damage){
-        // methode pour handle le damage taken by the hero
+        // methode pour handle le damage taken by the hero  
         this.health -= damage;
         if(this.health < 0){
             this.health = 0;
@@ -48,7 +69,44 @@ public class Hero {
 
     public int calculerExperience(){
         // methode pour pacluler l'experience pour atteindre le next level
+        // exp_requis = 50 + niveau_voulu * 20 * (1.1^niveau_voulu)
         return 50 + this.level * 20 * (int) Math.pow(1.1, this.level);   
     }
+    
+    public boolean fight(int numEnemies) {
+        // the fight action of the Hero
+        int totalDamage = numEnemies * this.attackPoints * 2; // Car c'est double damage
+        this.damageInflicted += totalDamage;
+        takeDamage(totalDamage);
+        return isAlive();
+    }
+
+    public boolean rest() {
+        // the rest action of the Hero
+        this.health = this.maxHealth; // restore health
+        return isAlive();
+    }
+
+    public boolean heal(int healthPoints){
+        // the heal action of the Hero
+        this.health += healthPoints;
+        if (this.health > this.maxHealth) {
+            this.health = this.maxHealth;
+        }
+        return isAlive();
+    }
+
+    public boolean train(int trainingPoints){
+        // handle the train action of the Hero
+        this.attackPoints += trainingPoints;
+        return isAlive();
+    }
+
+    public boolean isAlive(){
+        // pour checker if the hero is alive ou non
+        return this.health > 0;
+    }
+
+
 }
 
