58d07db2ca89fe19da516c7b70578eb265ead910
Soglo3006 <144140565+Soglo3006@users.noreply.github.com>
2024-02-21 19:44:59 -0500

    Update ArgsProcessor.java

--- src/ArgsProcessor.java
+++ src/ArgsProcessor.java
@@ -1,25 +1,35 @@
-public class ArgsProcessor {
+public class ArgsProcessor2 {
     public static void process(String[] args) {
         String[] phrase = makePhrase(args[0]);
-        //TODO : Handle the phrase and output the result
+
+        String nom = phrase[0];
+        int health = Integer.parseInt(phrase[1]);
+        int attackPoints = Integer.parseInt(phrase[2]);
+
+        Hero hero = new Hero(nom, health, health, 1, 0, attackPoints);
+
+        // TODO : Handle the phrase and output the result
     }
 
+    //
     private static String[] makePhrase(String args) {
         return args.trim().split(",");
     }
 
-    // méthode qui prend la partie de la phrase qui décrit l'action et le héros, puis effectue l'action correspondante
+    // méthode qui prend la partie de la phrase qui décrit l'action et le héros,
+    // puis effectue l'action correspondante
     // retourne true si le joueur survit à l'action, false sinon
     private static boolean doAction(String action, Hero hero) {
-        // ici, on transforme le String action en un tableau de String, en séparant les mots par des espaces
+        // ici, on transforme le String action en un tableau de String, en séparant les
+        // mots par des espaces
         String[] phrase = action.trim().split(" ");
         // le type d'action est déterminé par le premier mot de la phrase
         switch (phrase[0]) {
             case "fought":
-                //TODO : Handle the fight
-                break;
+                // TODO : Handle the fight
+                br eak;
             case "rested":
-                //TODO : Handle the resting
+                // TODO : Handle the resting
                 break;
             case "healed":
                 // TODO : Handle the healing
